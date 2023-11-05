f470d000d8096289541af20d536146f16496bcdc
Nicolain12 <lainnicolas1@gmail.com>
2023-11-05 17:08:40 -0300

    header done

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -10,18 +10,29 @@ function header({ nameClass }) {
     <div className="component-header">
       <header>
         <div className="h-logo-div">
-        <h1>Header</h1>
+          <h1>LOGO</h1>
         </div>
         <div className="h-links-div">
-          <Link className='h-links-options' to={'#'}>All Products</Link>
-          <Link className='h-links-options' to={'#'}>Hot sale</Link>
-          <Link className='h-links-options' to={'#'}>Contact us</Link>
+          <div className="h-links-inner-div">
+            <Link className='h-links-options' to={'#'}>All Products</Link>
+            <Link className='h-links-options' to={'#'}>Hot sale</Link>
+            <Link className='h-links-options' to={'#'}>Contact us</Link>
+          </div>
         </div>
         <div className="h-profile-menu-div">
-          <div className="h-profile-options-div">
+          {/* Not Logged */}
+          {/* <div className="h-profile-options-div">
+            <Link className='h-profile-links' to={'#'}>Loggin</Link>
             <Link className='h-profile-links' to={'#'}>Register</Link>
-            <Link className='h-profile-links' to={'#'}>Profile</Link>
+          </div> */}
+          {/* Logged */}
+          <div className="h-profile-options-div">
+            <Link className='h-profile-links' to={'#'}><i class="fa-solid fa-cart-shopping"></i></Link>
+            <Link className='h-profile-links' to={'#'}><i class="fa-regular fa-user"></i></Link>
           </div>
+        </div>
+        <div className="h-burgermenu-div">
+          <i class="fa-solid fa-bars"></i>
         </div>
       </header>
     </div>
